fdd934cde27488bdad54af35fa1c382faaf4d7c7
HuJie <13931154861@163.com>
2023-12-20 10:27:49 +0800

    init

---
+++ easy-swing-test/src/main/java/top/sysh/Main.java
@@ -0,0 +1,12 @@
+package top.sysh;
+
+
+/**
+ * @Author 董虎杰
+ * @Date 2023/12/20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
